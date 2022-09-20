dd6b4dc3e668cc6c49a012bff93700219ec9958a
Ilya Khorobrykh <horobryh17@gmail.com>
2022-09-20 17:01:02 +0300

    fixed url

--- src/api/auth/authAPI.ts
+++ src/api/auth/authAPI.ts
@@ -3,7 +3,7 @@ import { FormValuesType, UserDataType } from 'api/types';
 
 export const authAPI = {
     login: (userData: Omit<FormValuesType, 'fullName'>) => {
-        return instance.post<UserDataType>('auth/login', userData);
+        return instance.post<UserDataType>('/auth/login', userData);
     },
     me: () => {
         return instance.get<Omit<UserDataType, 'token'>>('auth/me');
